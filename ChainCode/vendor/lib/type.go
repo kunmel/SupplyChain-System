e26bcd8f7622f0e967652194923e9220c9c03ce3
maskva <44914344+maskva@users.noreply.github.com>
2021-06-11 12:36:23 +0800

    Update type.go

--- ChainCode/vendor/lib/type.go
+++ ChainCode/vendor/lib/type.go
@@ -1,24 +1,54 @@
 package lib
-type Order struct {//订单结构类型
-	Buyer Account //企业
-	Seller Account //供货商
-	GoodName Good//商品名称
-	GoodSum int//货品数量
-	OrderAmount int//订单总金额
-	IfAccept bool//是否接受？
-	CreatTime string//订单创建时间
-	Deadline string//截止日期？
-	Remark string//备注
+type Order struct {
+	ID             string
+	Buyer          string
+	Seller         string
+	BuyerID        string
+	SellerID       string
+	Address        string
+	GoodsID        string
+	Goodsname      string
+	Goodsnum       int
+	Orderamount    int
+	Createtime     string
+	Deadline       string
+	Remark         string
+	Transferstatus int
+	TransferID     string
+	Status         int
 }
 
-type Good struct {//货品结构类型
-	Name string //货品名称
-	Standards string //规格
-	Material string  //材料
-	Workmanship string  //制作工艺
-	Supplier Account //供应商
-	AddDate string //上架时间
-	Tel string  //联系方式
-	ShippingArea string  //发货地区
-	Remark string //备注
+type Product struct {
+	goodsID string
+	name string
+	goodtype string
+	amount int
+	price int
+	standards string
+	material string
+	workmanship string
+	supplier string
+	supplierID string
+	adddate string
+	tel string
+	shippingarea string
+	remark string
 }
+type usser struct {
+	ID string
+	account string
+	password string
+	identity int
+	name string
+}
+
+type financing struct {
+	supplier string
+	amount string
+	period string
+	desc string//申请描述
+	orderID string
+	status string// ("0":待审批 "1":审批通过 "2":审批拒绝)
+	feedback int
+}
+
