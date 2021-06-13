effa8fc091dd291c82041bbf999b2f38d6da8da6
maskva <44914344+maskva@users.noreply.github.com>
2021-06-13 23:01:21 +0800

    Update chaincode.go

--- ChainCode/chaincode.go
+++ ChainCode/chaincode.go
@@ -22,28 +22,7 @@ func (t *BlockChainRealEstate) Init(stub shim.ChaincodeStubInterface) pb.Respons
 	}
 	time.Local = timeLocal
 	//初始化默认数据
-	var accountIds = [6]string{
-		"5feceb66ffc8",
-		"6b86b273ff34",
-		"d4735e3a265e",
-		"4e07408562be",
-		"4b227777d4dd",
-		"ef2d127de37b",
-	}
-	var userNames = [6]string{"管理员", "①号业主", "②号业主", "③号业主", "④号业主", "⑤号业主"}
-	var balances = [6]float64{0, 5000000, 5000000, 5000000, 5000000, 5000000}
-	//初始化账号数据
-	for i, val := range accountIds {
-		account := &lib.Account{
-			AccountId: val,
-			UserName:  userNames[i],
-			Balance:   balances[i],
-		}
-		// 写入账本
-		if err := utils.WriteLedger(account, stub, lib.AccountKey, []string{val}); err != nil {
-			return shim.Error(fmt.Sprintf("%s", err))
-		}
-	}
+	
 	return shim.Success(nil)
 }
 
